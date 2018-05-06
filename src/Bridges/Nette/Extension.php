12d135740f456fddf3076314cfe4a897ceb9cf0c
Radek Fryšták <geniv.radek@gmail.com>
2018-05-06 21:17:18 +0200

    update extension

--- src/Bridges/Nette/Extension.php
+++ src/Bridges/Nette/Extension.php
@@ -4,8 +4,6 @@ namespace Macros\Bridges\Nette;
 
 use Macros\MacroConfirm;
 use Macros\MacroIfCurrent;
-use Macros\MacroIfCurrentIn;
-use Macros\MacroIfCurrentSwitch;
 use Macros\MacroSubmitButton;
 use Nette\DI\CompilerExtension;
 
@@ -29,8 +27,6 @@ class Extension extends CompilerExtension
         // load macro to latte
         $latteFactory = $builder->getDefinition('latte.latteFactory');
         $latteFactory->addSetup(MacroIfCurrent::class . '::install(?->getCompiler())', ['@self']);
-        $latteFactory->addSetup(MacroIfCurrentIn::class . '::install(?->getCompiler())', ['@self']);
-        $latteFactory->addSetup(MacroIfCurrentSwitch::class . '::install(?->getCompiler())', ['@self']);
         $latteFactory->addSetup(MacroSubmitButton::class . '::install(?->getCompiler())', ['@self']);
         $latteFactory->addSetup(MacroConfirm::class . '::install(?->getCompiler())', ['@self']);
     }
